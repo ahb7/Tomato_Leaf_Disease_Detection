Final Fix: TFLite working locally without normalization

app.py
@@ -4,6 +4,8 @@ from flask import Flask, request, render_template, redirect
 from werkzeug.utils import secure_filename
 from tensorflow.keras import models
 from PIL import Image
+import tensorflow as tf
+
 
 try:
     import markupsafe
@@ -24,7 +26,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # Load Model
-MODEL = models.load_model("./model")
+# MODEL = models.load_model("./model")
 CLASS_NAMES = [
     'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight',
     'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Target_Spot',
@@ -34,6 +36,14 @@ CLASS_NAMES = [
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output details.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -58,9 +68,11 @@ def index():
                 img = Image.open(filepath).convert('RGB')
                 img = img.resize((256, 256)) 
                 image = np.asarray(img)
-                img_batch = np.expand_dims(image, 0)
+                img_batch = np.expand_dims(image, 0).astype(np.float32)
 
-                prediction = MODEL.predict(img_batch)
+                interpreter.set_tensor(input_details[0]['index'], img_batch)
+                interpreter.invoke()
+                prediction = interpreter.get_tensor(output_details[0]['index'])
                 predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
                 confidence = round(100 * (np.max(prediction[0])), 2)
 